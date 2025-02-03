44f7790f800949f418a3b44b2389895e4d1cd32f
MhmdMshhwt <85801466+MhmdMshhwt@users.noreply.github.com>
2025-02-03 18:28:31 +0200

    first commit

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+
+export const gridItems = [
+    {title:'Title1', description:'Description1', id: '1'},
+    {title:'Title2', description:'Description2', id: '2'},
+    {title:'Title3', description:'Description3', id: '3'},
+    {title:'Title4', description:'Description4', id: '4'},
+]
